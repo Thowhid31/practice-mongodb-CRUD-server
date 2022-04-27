63ff8a546404961ca171d52614bc8d3a415c8d51
Thowhid31 <thowhidantor16@gmail.com>
2022-04-27 16:44:27 +0600

    added mongodeb and crud first aid

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const port= process.env.PORT || 5000;
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors())
+app.use(express.json());
+
+
+
+const uri = "mongodb+srv://user1:cruQjOXLzJfEPf7x@cluster0.wcunk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//node mongodb CRUD operation
+async function run(){
+    //js try catch finally
+    try{
+        await client.connect();
+        const userCollection = client.db("express").collection('user');
+        const user = {name: 'Maha Moha 2', email: 'janina2@gmail.com'}
+        const result = await userCollection.insertOne(user)
+        console.log(`user inserted with id: ${result.insertedId}`);
+    }
+    finally{
+        await client.close();
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res)=>{
+    res.send('Running My node CRUD Server')
+});
+
+app.listen(port, ()=>{
+    console.log('CRUD Server is Running', port);
+})
+
+
+
+//user: user1
+//pass: cruQjOXLzJfEPf7x
