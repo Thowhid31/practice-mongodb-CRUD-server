2beb19b03a0121130d55e8fa90f9d92a78c33c83
Thowhid31 <thowhidantor16@gmail.com>
2022-04-28 00:30:48 +0600

    implement all others features

--- index.js
+++ index.js
@@ -15,31 +15,74 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 //node mongodb CRUD operation
 async function run(){
     //js try catch finally
-    try{
-        await client.connect();
-        const userCollection = client.db("express").collection('user');
-        
-        app.post('/user', (req, res)=>{
-            const newUser = req.body;
-            console.log('adding new user.', newUser);
-            res.send({result: 'success'})
-        })
+        try{
+            await client.connect();
+            const userCollection = client.db('foodExpress').collection('user');
+    
+            // get users
+            app.get('/user', async(req, res) =>{
+                const query = {};
+                const cursor = userCollection.find(query);
+                const users = await cursor.toArray();
+                res.send(users);
+            });
+    
+            app.get('/user/:id', async(req, res) =>{
+                const id = req.params.id;
+                const query = {_id: ObjectId(id)};
+                const result = await userCollection.findOne(query);
+                res.send(result);
+            });
+    
+            // POST User : add a new user
+            app.post('/user', async(req, res) =>{
+                const newUser = req.body;
+                console.log('adding new user', newUser);
+                const result = await userCollection.insertOne(newUser);
+                res.send(result)
+            });
+    
+            // update user
+            app.put('/user/:id', async(req, res) =>{
+                const id = req.params.id;
+                const updatedUser = req.body;
+                const filter = {_id: ObjectId(id)};
+                const options = { upsert: true };
+                const updatedDoc = {
+                    $set: {
+                        name: updatedUser.name,
+                        email: updatedUser.email
+                    }
+                };
+                const result = await userCollection.updateOne(filter, updatedDoc, options);
+                res.send(result);
+    
+            })
+    
+            // delete a user
+            app.delete('/user/:id', async(req, res) =>{
+                const id = req.params.id;
+                const query = {_id: ObjectId(id)};
+                const result = await userCollection.deleteOne(query);
+                res.send(result);
+            })
+    
+        }
+        finally{
+    
+        }
     }
-    finally{
-        await client.close();
-    }
-}
-
-run().catch(console.dir)
-
-app.get('/', (req, res)=>{
-    res.send('Running My node CRUD Server')
-});
-
-app.listen(port, ()=>{
-    console.log('CRUD Server is Running', port);
-})
-
+    
+    run().catch(console.dir);
+    
+    
+    app.get('/', (req, res) =>{
+        res.send('Running My Node CRUD Server');
+    });
+    
+    app.listen(port, () =>{
+        console.log('CRUD Server is running');
+    })
 
 
 //user: user1
